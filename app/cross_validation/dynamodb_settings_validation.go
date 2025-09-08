d419ba1e623bd9bd335c9f02ea4439ff97f0ae7d
Ivan Meireles <ivanmds2@hotmail.com>
2025-09-08 14:13:09 -0300

    validate if the action has the sort key  configured when table has sort key

--- app/cross_validation/dynamodb_settings_validation.go
+++ app/cross_validation/dynamodb_settings_validation.go
@@ -3,9 +3,11 @@ package cross_validation
 import (
 	"fmt"
 	"wrench/app/manifest/action_settings"
+	"wrench/app/manifest/action_settings/dynamodb_settings"
 	"wrench/app/manifest/application_settings"
 	"wrench/app/manifest/connection_settings"
 	"wrench/app/manifest/validation"
+	"wrench/app/manifest_cross_funcs"
 	"wrench/app/startup/connections"
 )
 
@@ -20,6 +22,7 @@ func dynamodbCrossValidation(appSetting *application_settings.ApplicationSetting
 
 	if len(appSetting.Actions) > 0 {
 		result.Append(dynamoDbActionTableIdExist(appSetting.Actions))
+		result.Append(checkActionDynamoDbKeyConfiguredCorrect(appSetting.Actions))
 	}
 
 	return result
@@ -52,6 +55,34 @@ func dynamoDbActionTableIdExist(settings []*action_settings.ActionSettings) vali
 				if err != nil {
 					result.AddError(fmt.Sprintf("actions[%v].dynamodb.tableId don't exist in connections.dynamodb.tables", setting.Id))
 				}
+			}
+		}
+	}
+
+	return result
+}
+
+func checkActionDynamoDbKeyConfiguredCorrect(settings []*action_settings.ActionSettings) validation.ValidateResult {
+	var result validation.ValidateResult
+
+	if len(settings) > 0 {
+		for _, setting := range settings {
+			if setting.DynamoDb != nil {
+				if setting.DynamoDb.Command == dynamodb_settings.DynamoDbCommandGet ||
+					setting.DynamoDb.Command == dynamodb_settings.DynamoDbCommandDelete {
+
+					table, err := manifest_cross_funcs.GetDynamodbTableSettings(setting.DynamoDb.TableId)
+
+					if err == nil {
+						if len(table.SortKeyName) > 0 {
+							if setting.DynamoDb.Key == nil || len(setting.DynamoDb.Key.SortKeyValue) == 0 {
+								result.AddError(fmt.Sprintf("actions[%v].dynamodb.key.sortKeyValue is required when connections.dynamodb.tables[%v] have sort key", setting.Id, setting.DynamoDb.TableId))
+							}
+						}
+					}
+
+				}
+
 			}
 		}
 	}
