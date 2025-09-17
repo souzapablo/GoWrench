0153cfc54a91106a175959607a7689bb291329ae
Ivan Meireles <ivanmds2@hotmail.com>
2025-09-17 09:46:37 -0300

    move to package app

---
+++ app/telemetrics_func.go
@@ -0,0 +1,25 @@
+package app
+
+import (
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+var instanceID string
+
+func init() {
+	instanceID = loadInstanceID()
+}
+
+func loadInstanceID() string {
+	instance := uuid.New()
+	instanceId := instance.String()
+	instanceId = strings.ReplaceAll(instanceId, "-", "")
+
+	return instanceId
+}
+
+func GetInstanceID() string {
+	return instanceID
+}
